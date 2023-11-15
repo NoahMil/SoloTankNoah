d23ae1d5b56ffdfbbbe4535c4a1ac8a16045e8c7
NoahMil <noah.miliani13@gmail.com>
2023-11-15 15:53:26 +0100

    Save Implant

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,94 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+
+public class SaveSystem : MonoBehaviour
+{
+    public string saveName = "SaveData_";
+    [Range(0, 10)] public int saveDataIndex = 0;
+
+    public void SaveData(string dataToSave)
+    {
+        if (WriteToFile(saveName + saveDataIndex, dataToSave))
+        {
+            Debug.Log("Successfully saved data");
+        }
+    }
+
+    public string LoadData()
+    {
+        string data = "";
+        if (ReadFromFile(saveName + saveDataIndex, out data))
+        {
+            Debug.Log("Successfully loaded data");
+        }
+        return data;
+    }
+    
+
+    private bool WriteToFile(string name, string content)
+    {
+        var fullPath = Path.Combine(Application.persistentDataPath, name);
+
+        try
+        {
+            File.WriteAllText(fullPath, content);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error saving to a file " + e.Message);
+        }
+
+        return false;
+    }
+
+    private bool ReadFromFile(string name, out string content)
+    {
+        var fullPath = Path.Combine(Application.persistentDataPath, name);
+        try
+        {
+            content = File.ReadAllText(fullPath);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error when loading the file " + e.Message);
+            content = "";
+        }
+
+        return false;
+    }
+}
+/*public static void SaveCharacter(Tank character, string filePath)
+{
+    BinaryFormatter formatter = new BinaryFormatter();
+    FileStream stream = new FileStream(filePath, FileMode.Create);
+
+    CharacterMemento memento = character.SaveToMemento();
+
+    formatter.Serialize(stream, memento);
+    stream.Close();
+}
+
+public static void LoadCharacter(Tank character, string filePath)
+{
+    if (File.Exists(filePath))
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(filePath, FileMode.Open);
+
+        CharacterMemento memento = (CharacterMemento)formatter.Deserialize(stream);
+        stream.Close();
+
+        character.RestoreFromMemento(memento);
+    }
+    else
+    {
+        Debug.LogError("Save file not found");
+    }
+    */
+
+
+
--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -10,13 +10,15 @@ public class Tank : Base_Controller
     [SerializeField] private float Speed = 0.1f;
     [SerializeField] private float RotationSpeed = 0.1f;
     [SerializeField] private float BoostSpeed = 0.1f;
-    [SerializeField] private  float BoostDuration = 0.1f;
-    [SerializeField] private bool  IsBoosted = false ;
+    [SerializeField] private float BoostDuration = 0.1f;
+    [SerializeField] private bool IsBoosted = false;
+
+    public Vector3 _position { get; set; }
 
     public delegate void TankEvents();
 
     public static event TankEvents OnUpdateHealth;
-    
+
     private void Start()
     {
         if (AppData.InFirstScene)
@@ -24,49 +26,61 @@ public class Tank : Base_Controller
             _playerData.LifePoint = _playerData.MaxLifePoint;
         }
         OnUpdateHealth?.Invoke();
+
     }
 
     private void Update()
     {
         AimToTarget();
 
+        _position = transform.position;
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(0f, 0f, Speed*Time.deltaTime);
+            transform.Translate(0f, 0f, Speed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(0f, 0f, -Speed*Time.deltaTime);
+            transform.Translate(0f, 0f, -Speed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(0f, RotationSpeed*Time.deltaTime, 0f);
+            transform.Rotate(0f, RotationSpeed * Time.deltaTime, 0f);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(0f, -RotationSpeed*Time.deltaTime, 0f);
+            transform.Rotate(0f, -RotationSpeed * Time.deltaTime, 0f);
         }
         if (Input.GetMouseButton(0))
         {
             Fire();
         }
-    }
 
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SaveCharacter();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            LoadGame();
+        }
+        
+        
+    }
 
     private void AimToTarget()
     {
         Ray tempRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(tempRay,  out hit))
+        if (Physics.Raycast(tempRay, out hit))
         {
             RotateToTarget(hit.point);
-        } 
+        }
     }
-
-
 
     [SerializeField] private GameObject TurretTarget;
     private void OnTriggerEnter(Collider other)
@@ -77,7 +91,7 @@ public class Tank : Base_Controller
             StartCoroutine(Boost());
         }
     }
-    
+
     private IEnumerator Boost()
     {
         float tempSpeed = Speed;
@@ -86,17 +100,47 @@ public class Tank : Base_Controller
         Speed = tempSpeed;
         IsBoosted = false;
     }
-    
+
     public override void ApplyDamage(int damage)
     {
-
-        _playerData.LifePoint -=  damage;
+        _playerData.LifePoint -= damage;
         if (_playerData.LifePoint <= 0)
         {
             Destruction();
         }
         OnUpdateHealth?.Invoke();
+    }
 
+    public CharacterMemento SaveToMemento()
+    {
+        return new CharacterMemento(transform.position, _playerData.LifePoint);
+    }
+
+    public void RestoreFromMemento(CharacterMemento memento)
+    {
+        transform.position = memento.Position;
+        _playerData.LifePoint = memento.Health;
+    }
+
+    public void SaveCharacter()
+    {
+    }
+
+    private void LoadGame()
+    {
+    }
+}
+
+[Serializable]
+public class CharacterMemento
+{
+    public Vector3 Position;
+    public float Health;
+
+    public CharacterMemento(Vector3 position, float health)
+    {
+        Position = position;
+        Health = health;
     }
 }
 
